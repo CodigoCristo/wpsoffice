937b6c080c2f95d16f3ef80c7825dcff49c8867a
CodigoCristo <cristo@Arcris.localdomain>
2022-04-23 07:19:44 -0500

    Codigo Cristo

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 #fuente: https://extensions.libreoffice.org/en/extensions/show/spanish-dictionaries
 # RUTA EN /OPT/
-# ruta_program="/opt/kingsoft/wps-office/office6/mui/en_US"+
+# ruta_program="/opt/kingsoft/wps-office/office6/mui/en_US"
 
 idioma="es_MX"
 ruta_program="/usr/lib/office6/mui/en_US"
 ruta_dicts="/usr/lib/office6/dicts/spellcheck"
 
-
+sudo rm -rf ${ruta_program}/*
 sudo cp -rf mui_11074/${idioma}/* ${ruta_program}
 sudo cp -rf dicts/es_ES ${ruta_dicts}
